691bea0cc664f72667d5df059e1d424866306667
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2025-01-10 09:03:05 -0300

    feat: add header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Hero } from './components/landing-page/Hero';
+import { Header } from './components/landing-page/Header';
 
 export default function Home() {
   return (
     <div className="h-screen max-w-7xl mx-auto">
+      <Header />
       <Hero />
-      {/* <Header />
+      {/*
      
       <VideoExplanation />
       <Pricing />
